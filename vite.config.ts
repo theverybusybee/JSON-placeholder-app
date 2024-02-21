18e1d85d994423d86c15f73b6d74bdee2a6de668
theverybusybee <v_antonchik@mail.ru>
2024-02-21 20:00:59 +0300

    ci(node_modules): new devDependencies added
    new devDependencies added: husky, prettier, linter commitizen

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
+import viteTsconfigPaths from "vite-tsconfig-paths"
+import eslint from "vite-plugin-eslint"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/",
+  plugins: [react(), viteTsconfigPaths(), eslint()],
   server: {
     open: true,
+    port: 3000,
   },
   test: {
     globals: true,
