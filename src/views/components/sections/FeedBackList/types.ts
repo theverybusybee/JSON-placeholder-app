71245ae6734d4473e1dbf32b49fd7c9783af4b2a
theverybusybee <v_antonchik@mail.ru>
2024-02-24 21:58:41 +0300

    feat(feedbacklist): component added

---
+++ src/views/components/sections/FeedBackList/types.ts
@@ -0,0 +1,6 @@
+import type { FeedbackProps } from 'views/components/ui-components/Feedback/types';
+
+export interface FeedbackListProps {
+  feedbackList: FeedbackProps[];
+  extraClass?: string;
+}
