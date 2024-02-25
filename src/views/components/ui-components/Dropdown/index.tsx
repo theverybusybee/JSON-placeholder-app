c7fa9b1922a7f342964adc8a3985bdc1fc55ee4f
theverybusybee <v_antonchik@mail.ru>
2024-02-25 11:13:12 +0300

    build(dropdown): excessive code removed

--- src/views/components/ui-components/Dropdown/index.tsx
+++ src/views/components/ui-components/Dropdown/index.tsx
@@ -7,34 +7,22 @@ export const Dropdown: React.FC<DropdownProps> = ({
   users,
   onClick,
 }) => {
-  const [isDropDownActive, setIsDropDownActive] = useState(false);
-
-  const openDropDownMenu = () => {
-    setIsDropDownActive(true);
-  };
-
-  const closeDropDownMenu = () => {
-    setIsDropDownActive(false);
-  };
-
   return (
-    <article
-      className={`${styles.dropdown} ${extraClass}`}
-      onMouseOver={openDropDownMenu}
-      onMouseOut={closeDropDownMenu}
-    >
-      <ul className={styles.buttonList}>
-        {users.length > 0 &&
-          users.map((user) => {
-            return (
-              <li className={styles.buttonItem} key={user.id}>
-                <button className={styles.button} onClick={onClick}>
-                  {user.name}
-                </button>
-              </li>
-            );
-          })}
-      </ul>
+    <article className={styles.dropdown}>
+      <div className={`${styles.dropdownContent} ${extraClass}`}>
+        <ul className={styles.buttonList}>
+          {users.length > 0 &&
+            users.map((user) => {
+              return (
+                <li className={styles.buttonItem} key={user.id}>
+                  <button className={styles.button} onClick={onClick}>
+                    {user.name}
+                  </button>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </article>
   );
 };
--- src/views/components/ui-components/Dropdown/types.ts
+++ src/views/components/ui-components/Dropdown/types.ts
@@ -1,8 +1,7 @@
-import type { SyntheticEvent } from 'react';
 import type { User } from 'slices/postsTypes';
 
 export interface DropdownProps {
-  onClick?: (() => void) | ((e: SyntheticEvent) => void);
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   extraClass?: string;
   users: User[];
 }
