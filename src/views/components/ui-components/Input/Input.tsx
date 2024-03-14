5c36f089188fe49c534855e5b047f981bbd12612
theverybusybee <v_antonchik@mail.ru>
2024-03-15 02:56:42 +0300

    feat(input): can be used as input or textarea component via type prop

---
+++ src/views/components/ui-components/Input/Input.tsx
@@ -0,0 +1,35 @@
+import { type InputProps } from './type';
+import styles from './index.module.scss';
+
+export function Input({
+  name,
+  isRequired,
+  type,
+  value,
+  onChange,
+  placeholder,
+  minLength,
+  maxLength,
+  validationContent,
+}: InputProps) {
+  const CustomTag = type;
+
+  return (
+    <label className={styles.label} htmlFor={name}>
+      <CustomTag
+        className={styles.input}
+        type={type}
+        required={isRequired}
+        name={name}
+        defaultValue={value}
+        onChange={onChange}
+        minLength={minLength}
+        maxLength={maxLength}
+        placeholder={placeholder}
+      />
+      {validationContent && (
+        <span className={styles.input__error}>{validationContent}</span>
+      )}
+    </label>
+  );
+}
